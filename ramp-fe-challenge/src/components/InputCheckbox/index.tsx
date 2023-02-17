06d6df8d77628e63f3a426ba781b7d51aca65434
v-vinay <v.vinayvarma1@gmail.com>
2023-02-16 22:56:54 -0500

    Initial Commit with bug fixes

---
+++ ramp-fe-challenge/src/components/InputCheckbox/index.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        htmlFor={inputId} /* To associate the <label> with an <input> element, you need to give the <input> an id attribute. The <label> then needs a for attribute whose value is the same as the input's id*/
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  )
+}
